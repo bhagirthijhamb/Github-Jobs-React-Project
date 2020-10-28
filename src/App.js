154ff79236be9014bae35a0bc525f21413aaf76c
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-10-28 18:10:30 -0400

    First commit, added code to fetch from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import useFetchJobs from './useFetchJobs';
+import Job from './Job';
 
 function App() {
+  const [params, setParams ] = useState({ description: "python" });
+  const [page, setPage ] = useState(1);
+  const { jobs, loading, error} = useFetchJobs(params, page)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try refreshing</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+    </Container>
+    )
 }
 
 export default App;
